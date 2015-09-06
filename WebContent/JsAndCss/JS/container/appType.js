07d24c51f8a5f5ce4a1edcd71b3d760f212f1479
SKCoE18 <SKCoE18@SKCoE18-PC>
2015-09-06 23:44:01 +0700

    update

--- WebContent/JsAndCss/JS/container/appType.js
+++ WebContent/JsAndCss/JS/container/appType.js
@@ -1,65 +1,11 @@
-function xx (){
-	$.ajax({
-		method: "POST",
-		url: appCore.path+"/Core",
-		data:{"c":"{'service':'JSON','xx':'xxq'}"},
-		dataType: "json",
-		success:function(  data,  textStatus,  jqXHR ){
-
-			console.log( data ); // Data returned
-			console.log( textStatus ); // Success
-			console.log( jqXHR.status ); // 200
-			console.log( "Load was performed." );
-
+var app={
+		id:"",
+		buildHTML:function(){
+			$( "#"+this.id ).empty();
+			
 		},
-		error:function(jqXHR,  textStatus,  errorThrown ){
-			console.log( textStatus ); // Data returned
-			console.log( errorThrown ); // Success
-			console.log( jqXHR.status ); // 200
-		}
-	}); 
-
-	var $test = $( "#test" );
-	function handler1() {
-		console.log( "handler1:"+appCore.path );
-		$.getScript(appCore.path+"/Core", function(e) {
-			if(appCore.load){
-				appCore.load();
-				appCore.load= true;
-			}
-		});	
-	}						 
-	$test.on( "click", handler1 );
-
-
-	var test2 = $( "#test2" );
-	function handler2() {
-		$.ajax({
-			method: "POST",
-			url: appCore.path+"/Core",
-			data:{"c":"{'service':'LOAD_APP','xx':'xxq'}"},
-			dataType: "json",
-			success:function(  data,  textStatus,  jqXHR ){
-
-				console.log( data ); // Data returned
-				console.log( textStatus ); // Success
-				console.log( jqXHR.status ); // 200
-				console.log( "Load was performed." );
-
-			},
-			error:function(jqXHR,  textStatus,  errorThrown ){
-				console.log( textStatus ); // Data returned
-				console.log( errorThrown ); // Success
-				console.log( jqXHR.status ); // 200
-			}
-		}); 
-	}						 
-	test2.on( "click", handler2 );
-
-};
-
-/* 	$.ajax({
-										  method: "GET",
-										  url: appCore.path,
-										  dataType: "script"
-										}); */
+		buildCSS:function(){alert("buildCSS");},
+		buildEvent:function(){alert("buildEvent");},
+		data:{},
+		load:function(){alert("load");this.buildHTML();this.buildCSS();this.buildEvent();}
+	};
